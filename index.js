56ca2dee808af4aa2c6e682e112844e92a4d7286
Rahul-Verma <94426476+rvstarcaptain@users.noreply.github.com>
2022-03-27 22:05:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+ let cel = document.getElementById('celcius');
+        let far = document.getElementById('farhen');
+        cel.addEventListener('input',function(){
+            
+            let c= (cel.value * 9/5) + 32
+            far.value = c;
+        })
+        far.addEventListener('input',function(){
+            
+            if(far.value == false){
+               return cel.value= Number(0);
+            }else{
+                let f = ((far.value - 32) * 5/9 )
+                cel.value= f.toFixed(4);
+            }
+        })
